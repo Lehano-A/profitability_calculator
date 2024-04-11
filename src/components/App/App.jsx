e3ab5577c5ff8e2ad20f85b759d8c625360e409d
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-04-11 15:13:45 +0300

    Добавляет компонент CalculationInterestRate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import InputTypeDeposit from '../Form/InputTypeDeposit/InputTypeDeposit'
 import Header from '../Header/Header'
 import styled from 'styled-components'
 import InputInvestmentPeriod from '../Form/InvestmentPeriod/InvestmentPeriod'
+import CalculationInterestRate from '../Form/CalculationInterestRate/CalculationInterestRate'
 
 const CommonBox = styled.div`
   margin: 60px 20px 0;
@@ -20,6 +21,7 @@ function App() {
         <MonetaryUnit />
         <InputAmountInvestment />
         <InputInvestmentPeriod />
+        <CalculationInterestRate />
       </Form>
     </CommonBox>
   )
---
+++ src/components/Form/CalculationInterestRate/CalculationInterestRate.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+import Label from '../../App/Common/Form/Label/Label'
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 280px;
+  min-height: 106px;
+  height: 100%;
+  width: 100%;
+  border-radius: 15px;
+  background-color: ${(props) => props.theme.palette.shades.secondary[100]};
+`
+
+const BoxOutput = styled.div`
+  display: flex;
+  align-items: first baseline;
+`
+
+const Output = styled.output`
+  font-size: 2.4rem;
+  font-weight: 700;
+`
+
+const PercentDesignation = styled(Output)`
+  margin-right: 0.5rem; /* имитация пробела */
+`
+
+const UnitMeasurement = styled.span`
+  font-size: 1.4rem;
+  color: ${(props) => props.theme.palette.secondary};
+`
+
+function CalculationInterestRate() {
+  return (
+    <Box>
+      <Label>Процентная ставка</Label>
+
+      <BoxOutput>
+        <Output>0.75</Output>
+        <PercentDesignation as='span'>%</PercentDesignation>
+        <UnitMeasurement>в день</UnitMeasurement>
+      </BoxOutput>
+    </Box>
+  )
+}
+
+export default CalculationInterestRate
