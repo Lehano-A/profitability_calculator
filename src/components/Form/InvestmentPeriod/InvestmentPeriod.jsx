66877155e20714c64b1257c8980fec77545131ff
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-04-16 13:37:07 +0300

    Выносит InputRange в отдельный стайлинговый компонент

--- src/components/Form/InvestmentPeriod/InvestmentPeriod.jsx
+++ src/components/Form/InvestmentPeriod/InvestmentPeriod.jsx
@@ -1,32 +1,7 @@
 import styled from 'styled-components'
 import BoxSectionForm from '../../App/Common/Form/BoxSectionForm/BoxSectionForm'
 import Label from '../../App/Common/Form/Label/Label'
-import Input from '../../styled/Input'
-
-const InputRange = styled(Input)`
-  border: none;
-  padding: 0;
-  margin: 0 0 4px 0;
-
-  &::-webkit-slider-thumb {
-    width: ${(props) => props.theme.elements.inputRange.thumb.width}px;
-    height: ${(props) => props.theme.elements.inputRange.thumb.height}px;
-    padding: 19px 7.5px;
-    border-radius: 3px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-    background-color: #fff;
-    appearance: none;
-    margin-top: -${(props) => props.theme.elements.inputRange.thumb.height / 2 - props.theme.elements.inputRange.track.height / 2}px;
-  }
-
-  &::-webkit-slider-runnable-track {
-    background-color: ${(props) => props.theme.palette.primary};
-    border-radius: 30px;
-    height: ${(props) => props.theme.elements.inputRange.track.height}px;
-    appearance: none;
-    width: 100%;
-  }
-`
+import InputRange from '../../styled/InputRange'
 
 const Datalist = styled.datalist`
   display: flex;
