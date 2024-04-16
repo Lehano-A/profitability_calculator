828f4885e22a70b9a78ce0a5ebef039ab01f6cba
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-04-16 22:04:19 +0300

    Добавляет медиа-запрос 480px компоненту ButtonSubmit

--- src/components/Form/ButtonSubmit/ButtonSubmit.jsx
+++ src/components/Form/ButtonSubmit/ButtonSubmit.jsx
@@ -1,16 +1,25 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-  width: 280px;
-  height: 56px;
   border-radius: 0 90px;
   border: none;
   background-color: ${(props) => props.theme.palette.primary};
   text-transform: uppercase;
-  font-size: 1.4rem;
   font-weight: 900;
   letter-spacing: -1.66px;
   cursor: pointer;
+
+  @media (min-width: 320px) {
+    width: 280px;
+    height: 56px;
+    font-size: 1.4rem;
+  }
+
+  @media (min-width: 480px) {
+    width: 420px;
+    height: 70px;
+    font-size: 1.8rem;
+  }
 `
 
 function ButtonSubmit() {
