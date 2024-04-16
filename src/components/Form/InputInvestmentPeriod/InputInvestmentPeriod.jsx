91aeee2be604f8dce4ad538f38e7d76f65b0bc78
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-04-16 14:25:27 +0300

    Изменяет способ передачи внешнего отступа у компонента InputInvestmentPeriod, а также правит его файловое имя

---
+++ src/components/Form/InputInvestmentPeriod/InputInvestmentPeriod.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+import BoxSectionForm from '../../App/Common/Form/BoxSectionForm/BoxSectionForm'
+import Label from '../../App/Common/Form/Label/Label'
+import StyledInputRange from '../../styled/StyledInputRange'
+
+const Datalist = styled.datalist`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 1px 0 0;
+
+  & option {
+    width: ${(props) => props.theme.elements.inputRange.thumb.width}px;
+    color: ${(props) => props.theme.palette.shades.secondary[200]};
+    font-size: 14px;
+    font-weight: 500;
+    padding: 0;
+    text-align: center;
+  }
+`
+
+function InputInvestmentPeriod() {
+  return (
+    <BoxSectionForm id='componentInputInvestmentPeriod'>
+      <Label>Срок инвестиций ( Месяцев )</Label>
+
+      <StyledInputRange id='investmentPeriod' $margin='0 0 4px 0' type='range' min='1' max='12' list='range-list' />
+
+      <Datalist id='range-list'>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+        <option value='8'>8</option>
+        <option value='9'>9</option>
+        <option value='10'>10</option>
+        <option value='11'>11</option>
+        <option value='12'>12</option>
+      </Datalist>
+    </BoxSectionForm>
+  )
+}
+
+export default InputInvestmentPeriod
